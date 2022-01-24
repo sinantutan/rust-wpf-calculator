b514f1520593eafc84fb5294fa0c45c8e90fe67a
Sinan Tutan <73258913+sinantutan@users.noreply.github.com>
2022-01-24 22:58:47 +0100

    added new funcs to CalculatorViewModel

--- calc-ui-wpf/calc-ui-wpf/MainWindow.xaml.cs
+++ calc-ui-wpf/calc-ui-wpf/MainWindow.xaml.cs
@@ -1,21 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using calc_ui_wpf.ViewModels;
 
-
-namespace calc_ui_wpf 
+namespace calc_ui_wpf
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
--- calc-ui-wpf/calc-ui-wpf/ViewModels/CalculatorViewModel.cs
+++ calc-ui-wpf/calc-ui-wpf/ViewModels/CalculatorViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 namespace calc_ui_wpf.ViewModels
 {
@@ -11,8 +6,21 @@ namespace calc_ui_wpf.ViewModels
     {
         [DllImport("rust_calc.dll")]
         public static extern double add_f64(double number1, double number2);
+
+        [DllImport("rust_calc.dll")]
+        public static extern double sub_f64(double number1, double number2);
+
+        [DllImport("rust_calc.dll")]
+        public static extern double multi_f64(double number1, double number2);
+
+        [DllImport("rust_calc.dll")]
+        public static extern double div_f64(double number1, double number2);
+
         private double _num1;
         private double _num2;
+
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
 
         public double AddNumbers(double num1, double num2)
         {
